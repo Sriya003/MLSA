cbda37267c149cecd2280cc89fb65941c52dcedf
Sriya Smidha <mohapatrosriya123@gmail.com>
2023-10-11 20:24:54 +0530

    add changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/bck2.jpg";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Password Generator 🎉",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "A password strength generator using html,css and javascript",
+    url: "https://github.com/Sriya003/PasswordStrengthGenerator.git",
   },
   {
-    title: "Web Development for Beginners",
+    title: "HealthCARE one-stop",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Full stack Project for our GDSC WOW'23 Hackathon",
+    url: "https://github.com/Sriya003/GDSC-WOW-SDSB-GEEKS-.git",
   },
   {
     title: "My Resume Site",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Created from Html & Css resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
+    url: "https://github.com/Sriya003/MyPortfolio.git",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "E-Library",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "A project for my internship in ASCENT academy.",
+    url: "https://github.com/Sriya003/AscentAcademy_E_Library.git",
   },
 ];
 
